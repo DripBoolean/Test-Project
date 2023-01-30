a3a30be16865bbb762242810d8686caf36933b35
DripBoolean <simon.m.rhodes@gmail.com>
2023-01-30 11:34:26 -0500

    Got sfml working on linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Example.h>
+#include <SFML/Graphics.hpp>
 
 int main() {
 	std::cout << msg() << "\n";
@@ -7,6 +8,25 @@ int main() {
     Array<int> a(arr, 5);
     a.print();
     a.get(0);
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 	
 }
