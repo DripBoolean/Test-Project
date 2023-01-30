245cab7568c4793c25ed617fdedec781e4f51dfa
breadlofe <98194084+breadlofe@users.noreply.github.com>
2023-01-29 20:19:38 -0500

    Made a templated class for fun.

--- Include/Example.h
+++ Include/Example.h
@@ -1,4 +1,42 @@
 #pragma once
 #include <string>
+#include <iostream>
 
 std::string msg();
+
+template <typename T> class Array {
+private:
+    T* ptr;
+    int size;
+
+public:
+    Array(T arr[], int s);
+    void print();
+    void get(int index);
+};
+
+template <typename T> Array<T>::Array(T arr[], int s)
+{
+    ptr = new T[s];
+    size = s;
+    for (int i = 0; i < size; i++)
+        ptr[i] = arr[i];
+}
+
+template <typename T> void Array<T>::print()
+{
+    for (int i = 0; i < size; i++)
+        std::cout << " " << *(ptr + i) << std::endl;
+}
+
+template <typename T> void Array<T>::get(int index)
+{
+    if (index >= size)
+    {
+        throw std::out_of_range("Index given is out of range.");
+    }
+    else
+    {
+        std::cout << "Number at index " << index << " is: " <<  * (ptr + index) << std::endl;
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,4 +3,10 @@
 
 int main() {
 	std::cout << msg() << "\n";
+    int arr[5] = { 1, 2, 3, 4, 5 };
+    Array<int> a(arr, 5);
+    a.print();
+    a.get(0);
+    return 0;
+	
 }
