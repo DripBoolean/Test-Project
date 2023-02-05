d8ccbed914d19551a45c8514f9946f5ffb8e803e
breadlofe <breadlofe@gmail.com>
2023-02-04 20:03:22 -0500

    Doxygen strings to make GGJ people happy

--- Include/SFXlib.h
+++ Include/SFXlib.h
@@ -15,14 +15,46 @@ class SFXlib
 {
 private:
 
+/**
+ * @brief Vector containing type sf::Sound
+ * 
+ */
 vector<sf::Sound> sound;
+
+/**
+ * @brief Array containing type sf::SoundBuffer; array is length of ASSETS_LENGTH (number of enums)
+ * 
+ */
 sf::SoundBuffer buffer_array[ASSETS_LENGTH];
 
+/**
+ * @brief Map containing enumerated integers that represent different .wav files
+ * 
+ */
 map<int, string> SFX = {{assets::MOVING_ROOT_SFX, "assets/growing_root.wav",},
                                                         };
-
 public:
+    /**
+     * @brief Construct a new SFXlib object
+     * 
+     */
     SFXlib();
+
+    /**
+     * @brief Loads buffer for selected sound effect map and adds it to buffer array.
+     *        Function should be called once at beggining of main
+     * 
+     * @param SFX_selection int (probably enumurated) that represents mapped .wav file
+     */
     void load(int SFX_selection);
+
+    /**
+     * @brief First checks if any sound is playing currently. If it is, it moves to next
+     *        sound type in vector until it finds one that is not playing in specified buffer
+     *        and plays that one. If there are no sounds in the buffer, 
+     *        it will add one and play it.
+     * 
+     * @param SFX_selection int (probably enumurated) that represents mapped .wav file
+     */
     void play_SFX(int SFX_selection);
 };
